8270945bfc88ef21654814dff96b1a0aeafd9577
WoturmeloonRUN <143898154+HardikYadav99@users.noreply.github.com>
2024-09-20 03:39:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ with open('data/data_movies.json', 'r') as f:
 
 @app.route("/", methods=['GET'])
 def home():
-    return "Hi! This app is an API, there is no UI. Quantum mechanics ;-)"
+    return "Hi! This app is an API, there is no UI. Quantum mechanics! ;-)"
 
 
 @app.route('/discover')
